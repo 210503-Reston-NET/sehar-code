44e6551292df02965a564ac36fcbc8b0a5de2ba5
sehar-malik <sehar.malik508@gmail.com>
2021-05-04 13:16:13 -0700

    print reviews of restaurant

---
+++ 1.csharp/ResturantsReviews/RRUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using RRModel;
+namespace RRUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Restaurants goodTaste = new Restaurants("Good Taste", "Lahore", "Punjab");
+            goodTaste.Review = new Review {
+                Rating = 5,
+                Description = "A M A Z I N G"
+            };
+            Console.WriteLine(goodTaste.ToString());
+        }
+    }
+}
